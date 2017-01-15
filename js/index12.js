a3b08ced788f5a7d25651ebec31a991ddc5c436c
bjatta <me@bjatta.ru>
2017-01-16 01:06:22 +0300

    jQuery lessons
    work with localStorage and try to use maximum jQuery syntax.

---
+++ js/index12.js
@@ -0,0 +1,23 @@
+function twelve(ev) {
+  var toLocalStorage = [];
+
+  $('li>input:checked').each(function (){toLocalStorage.push(this.parentNode.innerHTML);});
+  $('p').text(' total: ' + $('li').length + ' done: ' + $('li>input:checked').length);
+  localStorage.setItem('ToDoList.BelHard',JSON.stringify(toLocalStorage));
+  localStorage.setItem('ToDoList.BelHard.allUl',JSON.stringify(document.querySelector('ul').innerHTML));
+}
+
+function taskDo(event){
+  $('<li>').text($('input:first').val()).appendTo('ul').prepend($('<input>',{'type':'checkbox'}));
+  $('li>input:checkbox').trigger('change');
+}
+
+$('<button>',{text:'-'}).prependTo('body').on('click',function(){$('li>input:checked').parent().remove();});
+$('<button>',{text:'+'}).prependTo('body').on('click',taskDo);
+$('<input>',{
+    type:'text',
+    placeholder:'+ add U task here...'
+  }).prependTo('body');
+
+$('ul').on('change','li',twelve).html(JSON.parse(localStorage.getItem('ToDoList.BelHard.allUl')));
+
