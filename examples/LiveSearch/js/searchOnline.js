e7e53a2920a1e49ffef096504e6bde6dd75943a6
bjatta <me@bjatta.ru>
2017-01-17 20:03:18 +0300

    Add files via upload

---
+++ examples/LiveSearch/js/searchOnline.js
@@ -0,0 +1,120 @@
+function addNewNameFromInput(){
+  var textContent = document.querySelector('input[id="search"]').value;
+  addNewName(textContent);
+}
+
+function searchOnline(ev){
+  var searchablePattern = '';
+  var cityesList = [];
+  var foundPosition = -1;
+  var hihglightedText = '';
+  var changeablePattern = '';
+  var foundTimes = new makeCounter();
+  var textForTooltip = '';
+
+    searchablePattern = ev.target.value;
+    if (ev.keyCode === 13) addNewNameFromInput();
+    cityesList = [].slice.call(document.getElementsByTagName('h5'));
+    cityesList.forEach(function (el){
+      el.innerHTML = el.innerHTML.replace('<span style="color: yellow;">','');            
+      el.innerHTML = el.innerHTML.replace('</span>','');
+      foundPosition = el.textContent.toLowerCase().indexOf(searchablePattern.toLowerCase())+1;
+      if (foundPosition && searchablePattern) {
+        changeablePattern = el.innerHTML.slice(foundPosition-1,foundPosition-1+searchablePattern.length);
+        hihglightedText ='<span style="color: yellow;">'+changeablePattern+'</span>';
+        el.parentNode.parentNode.className='btn btn-default active';
+        el.innerHTML=el.innerHTML.replace(changeablePattern,hihglightedText);
+        foundTimes();
+      }
+      else {
+        el.parentNode.parentNode.className='btn btn-default';
+      }
+    });
+    stringWithSearch = document.querySelector('div.tooltip');
+    if (searchablePattern) {
+      stringWithSearch.className = 'tooltip active';
+      textForTooltip = 'найдено: '+consilienceWord(foundTimes())+'...';
+      stringWithSearch.innerHTML = textForTooltip+'<div class="arrow"></div>';
+      stringWithSearch.style.width = '"'+(textForTooltip.length-2)+'ex"';
+    } else {stringWithSearch.className='tooltip';};
+  
+  }
+
+function mainInitial(){
+  var allNames = [].slice.call(arguments);
+  allNames.forEach(addNewName);
+}
+
+var myInput = document.getElementById('search');
+
+myInput.addEventListener('keyup',searchOnline);
+
+mainInitial('Афганистан','США','Германия','Россия','Беларусь','Канада','Варшава','Амстердам','Париж','Дамаск','ОАЭ','Казахстан','Замулко','Виктор','Иванович','БелХард','Преподаватель','Дорошевич','Юрий','Михайлович');
+
+function addNewName(textContent){
+  var mainDiv = document.querySelector('div.searchable-container');
+  var newDiv = {};
+  
+    newDiv = document.createElement('div');
+    newDiv.setAttribute('class','items col-xs-5 col-sm-5 col-md-3 col-lg-3');
+    mainDiv.appendChild(newDiv);
+    mainDiv = newDiv;
+    newDiv = document.createElement('div');
+    newDiv.setAttribute('class','info-block block-info clearfix');
+    mainDiv.appendChild(newDiv);
+    mainDiv = newDiv;
+    newDiv = document.createElement('div');
+    newDiv.setAttribute('class','square-box pull-left');
+    mainDiv.appendChild(newDiv);
+    var workDiv = mainDiv;
+    var newSpan = document.createElement('span');
+    newSpan.className='glyphicon glyphicon-tags glyphicon-lg';
+    newDiv.appendChild(newSpan);
+    mainDiv = workDiv;
+    newDiv = document.createElement('div');
+    newDiv.setAttribute('data-toggle','buttons');
+    newDiv.setAttribute('class','btn-group bizmoduleselect');
+    mainDiv.appendChild(newDiv);
+    mainDiv = newDiv;
+    newDiv = document.createElement('label');
+    newDiv.setAttribute('class','btn btn-default');
+    mainDiv.appendChild(newDiv);
+    mainDiv = newDiv;
+    newDiv = document.createElement('div');
+    newDiv.setAttribute('class','bizcontent');
+    mainDiv.appendChild(newDiv);
+    mainDiv = newDiv;
+    newDiv = document.createElement('input');
+    newDiv.setAttribute('type','checkbox');
+    newDiv.setAttribute('name','var_id[]');
+    newDiv.setAttribute('autocomplete','off');
+    newDiv.setAttribute('value','');
+    mainDiv.appendChild(newDiv);
+    newSpan = document.createElement('span');
+    newSpan.className ='glyphicon glyphicon-ok glyphicon-lg';
+    mainDiv.appendChild(newSpan);
+    newDiv = document.createElement('h5');
+    mainDiv.appendChild(newDiv);
+    newDiv.textContent = textContent;
+}
+
+function consilienceWord (number) {
+  var numberForWord = 0;
+
+  numberForWord = number;
+  if (numberForWord > 100) numberForWord = number % 100;
+  if (numberForWord > 20) numberForWord = number % 10;
+  switch (numberForWord) {
+    case 1: return ''+number+' совпадение';
+    case 2:
+    case 3:
+    case 4: return ''+number+' совпадения';
+    default: return ''+number+' совпадений';
+  }
+
+}
+
+function makeCounter(){
+  var counter=0;
+  return function () {return counter++};
+}
---
+++ examples/ToDo.list/js/todo.list.js
@@ -0,0 +1,86 @@
+(function(){
+/*
+Initial value for effects of animation
+*/
+var typeOffAnimatedDelete   = 'bounceOutRight';// archiving
+var typeOffAnimatedEffect 	= 'bounceInDown';  // appearance
+var typeOffAnimatedChecked	= 'bounceIn';      // selection (press or focus)
+var buttonWidthPercent      = '10%';           // size for button around input
+var animationTime           = 800;
+var div                     = 'div.bjattaToDoList';
+var liInputs                = 'div.bjattaToDoList > ul.bjattaToDoList > li > input';
+var li                      = 'div.bjattaToDoList > ul.bjattaToDoList > li';
+
+function twelve() {                            // count and make lower status string  
+  $('span.infoField').text(' total: '
+    + $(liInputs).length
+    + ' done: ' + $(liInputs+':checked').length);
+  localStorage.setItem('ToDoList.BelHard.allUl',JSON.stringify(document.querySelector(div+'>ul.bjattaToDoList').innerHTML));
+}
+
+function toggleInputCheckbox (ev) {
+  if (ev)
+    if (!ev.currentTarget.firstChild.checked)
+      $(ev.currentTarget.firstChild).attr('checked','checked').parent().removeClass(typeOffAnimatedEffect).addClass(typeOffAnimatedChecked).addClass('checked');
+    else
+      $(ev.currentTarget.firstChild).removeAttr('checked','checked').parent().removeClass(typeOffAnimatedChecked).addClass(typeOffAnimatedEffect).removeClass('checked');
+    pulseOn(ev.currentTarget);
+    setTimeout(pulseOff,animationTime,ev.currentTarget);
+    setTimeout(twelve,animationTime*1.01);
+}
+
+function span () {                             // Just add span (for compacting code only). Add span before all elements in div.
+  $('<span>',{html:'&nbsp'}).prependTo(div);
+}
+
+function addTask(ev){
+  var textForNewItem = $(div+' > input:text').val();
+  if (textForNewItem.length)
+    $('<li>',{text:textForNewItem, class:'roundedTwo bounceInDown animated'})
+      .appendTo(div+'>ul')
+      .prepend($('<input>',{'type':'checkbox'}))
+      .append($('<label>'))
+      .on('click',toggleInputCheckbox);
+    pulseOn(ev.target);
+    setTimeout(pulseOff,animationTime,ev.target);
+    setTimeout(twelve,animationTime*1.01);
+  $(div+' > input:text').val('');// clearing main input field when new task added
+}
+
+function pulseOff (ev) {  $(ev).removeClass(typeOffAnimatedChecked+' animated');}
+function pulseOn  (ev) {  $(ev).addClass(typeOffAnimatedChecked+' animated');}
+
+function deleteOff() {
+  var checkedLi = $(liInputs+':checked').parent();
+  for (var i = checkedLi.length - 1; i >= 0; i--) {
+    setTimeout(function(el){$(el).addClass(typeOffAnimatedDelete+' animated');},i*150,checkedLi[i]);
+    setTimeout(function(el){$(el).remove();twelve();},(i-1)*150+animationTime*1.05,checkedLi[i])
+  }
+}
+
+$('<button>',{text:'»',width: buttonWidthPercent,padding: '2px'})
+  .prependTo(div)
+  .on('click',deleteOff);
+
+span();
+
+$('<input>',{
+    type:'text',
+    placeholder:' just type U task here...',
+    width: '70%',
+    padding: '5px'
+  }).prependTo(div)
+  .on('keypress',function (ev) { if (13 === ev.keyCode) addTask(ev);})
+  .on('focus',pulseOn)
+  .on('blur',pulseOff);
+
+span();
+
+$('<button>',{text:'+',width: buttonWidthPercent,padding: '2px'}).prependTo(div).on('click',addTask);
+
+span();
+
+$(div+'>ul.bjattaToDoList').on('change','li',twelve).html(JSON.parse(localStorage.getItem('ToDoList.BelHard.allUl')));
+twelve();
+$(li).on('click',toggleInputCheckbox);
+})();
